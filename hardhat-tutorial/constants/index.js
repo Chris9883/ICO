4272326324ad6a81c6f0a50f39c9a3ff4ac86838
Christina <christina.edokam@mailbox.tu-dresden.de>
2022-06-21 00:14:11 +0200

    Initial commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x5Bd1101b894D85D58331891Df8f65B84463757ad";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
